9184d607f6bd2c155a3b388f2c255b0c02e67689
David Benkner <d.benkner@gmail.com>
2023-08-14 21:35:02 -0400

    added bootcamp table

--- Boot-Camp-Table/BootCamp-Table-Queries.sql
+++ Boot-Camp-Table/BootCamp-Table-Queries.sql
@@ -36,6 +36,57 @@ CREATE TABLE Curriculum (
     Day int not null,
     Topic varchar(50) not null,
     TechnologiesID int not NULL FOREIGN KEY REFERENCES Technologies(ID)
-
 );
+
+CREATE TABLE ASSESMENTS (
+    ID int Primary KEY Identity(1,1),
+    AssesmentName varchar(50) not null,
+    AssesmentGrade int null,
+    CurriculumID int not null FOREIGN KEY REFERENCES Curriculum(ID),
+    StudentID int not null FOREIGN KEY REFERENCES Students(ID)
+);
+
+insert Technologies (Name, InstructionDays)
+VALUES  
+    ('Git/Github', 2),
+    ('SQL', 6),
+    ('C#', 20),
+    ('Java', 6),
+    ('Hosting', 3),
+    ('HTML/CSS', 2),
+    ('JavaScript', 3),
+    ('Angular/TS', 5),
+    ('Interview Prep', 4),
+    ('Scrum', 2)
+
+Insert Curriculum (Day, Topic, TechnologiesID)
+VALUES 
+    (1, 'GitHub', 1),
+    (1, 'GIT', 1),
+    (3, 'SQL Searching DB', 2),
+    (3, 'SQL Creating and Editing Tables', 2),
+    (2, 'C# Basics', 3),
+    (1, 'Gallup Strengths', 9);
+select * from Curriculum
+
+ALTER TABLE ASSESMENTS
+    DROP Column CurriculumID;
+
+ALTER TABLE ASSESMENTS
+    ADD TechnologiesID int not NULL FOREIGN KEY REFERENCES Technologies(ID);
+
+Insert ASSESMENTS (AssesmentName, AssesmentGrade, StudentID, TechnologiesID)
+VALUES
+    ('Github Assessment', 110, 1, 1),
+    ('Github Assesment', 100, 2, 1),
+    ('Github Assesment', 50, 4, 1),
+    ('Github Assesment', 110, 5, 1)
 */
+
+select s.Firstname + ' ' + s.lastname as StudentName, a.assesmentname, a. assesmentgrade, t.Name TechnologyName
+from students s
+join ASSESMENTS a
+    on s.id = a.StudentID 
+join technologies t
+    on a.technologiesID = t.id
+
